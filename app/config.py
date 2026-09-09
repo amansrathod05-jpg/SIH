94306f1550dcccf546791bb8a00eea4396e0f7e1
Aman Rathod <amansrathod05@gmail.com>
2026-09-09 14:21:28 +0530

    Add configuration class for application settings

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = 'super-secret-hackathon-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/91942/OneDrive/Desktop/Smriti-Setu-SIH/backend/instance/smriti.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
